7daed017e46b4705b4dbec7979e1dacff7fed6f7
berk cirak <atalayberkcrak@gmail.com>
2025-06-04 15:45:30 +0300

    connected database

---
+++ backend/sellmate/src/main/java/com/example/sellmate/SellmateApplication.java
@@ -0,0 +1,13 @@
+package com.example.sellmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellmateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellmateApplication.class, args);
+	}
+
+}
---
+++ backend/sellmate/src/test/java/com/example/sellmate/SellmateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sellmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellmateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
