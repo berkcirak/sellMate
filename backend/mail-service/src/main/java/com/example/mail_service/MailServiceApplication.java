c612a4495b5419f73af4fd3760e0a4cb01902a9d
berk cirak <atalayberkcrak@gmail.com>
2025-07-04 12:59:26 +0300

    mailseervice added

---
+++ backend/mail-service/src/main/java/com/example/mail_service/MailServiceApplication.java
@@ -0,0 +1,18 @@
+package com.example.mail_service;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailServiceApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailServiceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("running...");
+	}
+}
