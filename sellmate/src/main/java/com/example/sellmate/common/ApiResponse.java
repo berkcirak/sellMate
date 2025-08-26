0574600b0bc9044133c972de39e8bd1b68fa7218
berk cirak <atalayberkcrak@gmail.com>
2025-08-26 03:57:58 +0300

    feat: added apiresponse

--- sellmate/src/main/java/com/example/sellmate/common/ApiResponse.java
+++ sellmate/src/main/java/com/example/sellmate/common/ApiResponse.java
@@ -1,2 +1,32 @@
-package com.example.sellmate.common;public class ApiResponse {
+package com.example.sellmate.common;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+import java.time.LocalDateTime;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record ApiResponse<T>(
+        boolean success,
+        String message,
+        T data,
+        LocalDateTime timestamp,
+        String path
+) {
+    public static <T> ApiResponse<T> success(T data){
+        return new ApiResponse<>(true, "Operation completed successfully",data, LocalDateTime.now(), null);
+    }
+    public static <T> ApiResponse<T> success(T data, String message){
+        return new ApiResponse<>(true, message, data, LocalDateTime.now(), null);
+    }
+    public static <T> ApiResponse<T> success(T data, String message, String path){
+        return new ApiResponse<>(true, message, data, LocalDateTime.now(), path);
+    }
+    public static <T> ApiResponse<T> error(String message){
+        return new ApiResponse<>(false, message, null, LocalDateTime.now(), null);
+    }
+    public static <T> ApiResponse<T> error(String message, String path){
+        return new ApiResponse<>(false, message, null, LocalDateTime.now(), path);
+    }
+
+
 }
