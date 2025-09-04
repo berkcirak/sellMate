6864711c01c8be3f233fd83940d090ece1d190b1
berk cirak <atalayberkcrak@gmail.com>
2025-09-05 01:58:33 +0300

    feat: added repo and mapper

---
+++ sellmate/src/main/java/com/example/sellmate/mapper/CommentMapper.java
@@ -0,0 +1,46 @@
+package com.example.sellmate.mapper;
+
+import com.example.sellmate.dto.request.CreateCommentRequest;
+import com.example.sellmate.dto.request.UpdateCommentRequest;
+import com.example.sellmate.dto.response.CommentResponse;
+import com.example.sellmate.dto.response.UserResponse;
+import com.example.sellmate.entity.Comment;
+import com.example.sellmate.entity.Post;
+import com.example.sellmate.entity.User;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CommentMapper {
+
+    public Comment toEntity(CreateCommentRequest request, User user, Post post){
+        Comment comment = new Comment();
+        comment.setContent(request.content());
+        comment.setPost(post);
+        comment.setUser(user);
+        return comment;
+    }
+    public CommentResponse toResponse(Comment comment){
+        UserResponse userResponse = new UserResponse(
+                comment.getUser().getId(),
+                comment.getUser().getFirstName(),
+                comment.getUser().getLastName(),
+                comment.getUser().getEmail(),
+                comment.getUser().getProfileImage(),
+                comment.getUser().getCreatedAt(),
+                comment.getUser().getUpdatedAt()
+        );
+        return new CommentResponse(
+                comment.getId(),
+                comment.getContent(),
+                userResponse,
+                comment.getPost().getId(),
+                comment.getCreatedAt(),
+                comment.getUpdatedAt()
+        );
+    }
+    public void updateEntityFromRequest(UpdateCommentRequest request, Comment comment){
+        comment.setContent(request.content());
+    }
+
+
+}
---
+++ sellmate/src/main/java/com/example/sellmate/repository/CommentRepository.java
@@ -0,0 +1,9 @@
+package com.example.sellmate.repository;
+
+import com.example.sellmate.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+}
