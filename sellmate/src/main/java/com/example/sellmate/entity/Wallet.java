b6cd8ecafc009002267db527839e5da47d687661
berk cirak <atalayberkcrak@gmail.com>
2025-09-09 13:34:53 +0300

    feat: added getter setters and business logic methods

--- sellmate/src/main/java/com/example/sellmate/entity/Wallet.java
+++ sellmate/src/main/java/com/example/sellmate/entity/Wallet.java
@@ -1,10 +1,7 @@
 package com.example.sellmate.entity;
 
 import com.example.sellmate.entity.base.BaseEntity;
-import jakarta.persistence.Entity;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 
@@ -14,11 +11,40 @@ public class Wallet extends BaseEntity {
 
 
     @OneToOne
-    @JoinColumn(name = "user_id")
+    @JoinColumn(name = "user_id", nullable = false, unique = true)
     private User user;
+    @Column(nullable = false, precision = 19, scale = 2)
     private BigDecimal balance = BigDecimal.ZERO;
+    @Column(nullable = false, length = 3)
     private String currency= "TRY";
 
+    public User getUser() {
+        return user;
+    }
 
+    public void setUser(User user) {
+        this.user = user;
+    }
 
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+    public void deposit(BigDecimal amount){
+        this.balance = balance.add(amount);
+    }
+    public void withdraw(BigDecimal amount){
+        this.balance = balance.subtract(amount);
+    }
 }
