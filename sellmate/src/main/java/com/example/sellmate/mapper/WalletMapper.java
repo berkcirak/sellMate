d5d6a3b529eb296c9e9d0b2edfdda987b1db38a8
berk cirak <atalayberkcrak@gmail.com>
2025-09-09 16:45:34 +0300

    feat: creater mapper class for wallet

--- sellmate/src/main/java/com/example/sellmate/mapper/WalletMapper.java
+++ sellmate/src/main/java/com/example/sellmate/mapper/WalletMapper.java
@@ -1,2 +1,12 @@
-package com.example.sellmate.mapper;public class WalletMapper {
+package com.example.sellmate.mapper;
+
+import com.example.sellmate.dto.response.WalletResponse;
+import com.example.sellmate.entity.Wallet;
+import org.springframework.stereotype.Component;
+
+@Component
+public class WalletMapper {
+    public WalletResponse toResponse(Wallet wallet){
+        return new WalletResponse(wallet.getBalance(), wallet.getCurrency());
+    }
 }
