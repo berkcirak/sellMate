8b1d5a408eff9b58e884084f22772f2d053bae39
berk cirak <atalayberkcrak@gmail.com>
2025-09-28 14:07:38 +0300

    feat: added rabbitmq config

--- sellmate/src/main/java/com/example/sellmate/config/RabbitMQConfig.java
+++ sellmate/src/main/java/com/example/sellmate/config/RabbitMQConfig.java
@@ -1,2 +1,28 @@
-package com.example.sellmate.config;public class RabbitMQConfig {
+package com.example.sellmate.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+
+    @Bean
+    public Queue welcomeEmailQueue(){
+        return QueueBuilder.durable("welcome.email.queue").build();
+    }
+    @Bean
+    public TopicExchange emailExchange(){
+        return new TopicExchange("email.exchange");
+    }
+    @Bean
+    public Binding welcomeEmailBinding(){
+        return BindingBuilder
+                .bind(welcomeEmailQueue())
+                .to(emailExchange())
+                .with("email.welcome");
+    }
+
+
 }
