c498bbb3f42a61c6ec53c9a875276cd77872a0f2
berk cirak <atalayberkcrak@gmail.com>
2025-10-02 02:06:08 +0300

    feat: added mapper class for conversation

--- sellmate/src/main/java/com/example/sellmate/mapper/ConversationMapper.java
+++ sellmate/src/main/java/com/example/sellmate/mapper/ConversationMapper.java
@@ -1,2 +1,26 @@
-package com.example.sellmate.mapper;public class ConversationMapper {
+package com.example.sellmate.mapper;
+
+import com.example.sellmate.dto.request.CreateConversationRequest;
+import com.example.sellmate.dto.response.ConversationResponse;
+import com.example.sellmate.entity.Conversation;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ConversationMapper {
+
+    public ConversationResponse toResponse(Conversation conversation){
+        return new ConversationResponse(
+                conversation.getId(),
+                conversation.getUserAId(),
+                conversation.getUserBId(),
+                conversation.getCreatedAt()
+        );
+    }
+    public Conversation toEntity(CreateConversationRequest request){
+        Conversation conversation = new Conversation();
+        conversation.setUserAId(request.userAId());
+        conversation.setUserBId(request.userBId());
+        return conversation;
+    }
+
 }
