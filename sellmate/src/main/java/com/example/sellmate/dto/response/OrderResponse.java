02de4a8e933cfe0a04882d908c9c61192e050a41
berk cirak <atalayberkcrak@gmail.com>
2025-10-10 18:46:02 +0300

    feat: added new methods

--- sellmate/src/main/java/com/example/sellmate/dto/response/OrderResponse.java
+++ sellmate/src/main/java/com/example/sellmate/dto/response/OrderResponse.java
@@ -1,2 +1,18 @@
-package com.example.sellmate.dto.response;public record OrderResponse() {
+package com.example.sellmate.dto.response;
+
+import com.example.sellmate.entity.enums.OrderStatus;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record OrderResponse(
+        Long id,
+        Long buyerId,
+        Long sellerId,
+        Long postId,
+        BigDecimal price,
+        OrderStatus status,
+        LocalDateTime createdAt,
+        LocalDateTime updatedAt
+) {
 }
