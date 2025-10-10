aa8e78506790c03dcd50b9e7255f0916fbba554a
berk cirak <atalayberkcrak@gmail.com>
2025-10-10 12:53:27 +0300

    feat: added order entity

---
+++ sellmate/src/main/java/com/example/sellmate/entity/Order.java
@@ -0,0 +1,67 @@
+package com.example.sellmate.entity;
+
+import com.example.sellmate.entity.base.BaseEntity;
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "orders")
+public class Order extends BaseEntity {
+
+    @Column(nullable = false)
+    private Long buyerId;
+    @Column(nullable = false)
+    private Long sellerId;
+    @Column(nullable = false)
+    private Long postId;
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal price;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private OrderStatus status = OrderStatus.COMPLETED;
+
+    public Long getBuyerId() {
+        return buyerId;
+    }
+
+    public void setBuyerId(Long buyerId) {
+        this.buyerId = buyerId;
+    }
+
+    public Long getSellerId() {
+        return sellerId;
+    }
+
+    public void setSellerId(Long sellerId) {
+        this.sellerId = sellerId;
+    }
+
+    public Long getPostId() {
+        return postId;
+    }
+
+    public void setPostId(Long postId) {
+        this.postId = postId;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+
+    public enum OrderStatus{
+        COMPLETED, CANCELED
+    }
+}
