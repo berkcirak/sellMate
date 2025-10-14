a33a1c26419a9d3135bac6355c4cfdefbacba478
berk cirak <atalayberkcrak@gmail.com>
2025-10-14 21:36:34 +0300

    feat: added repo layer for offer

--- sellmate/src/main/java/com/example/sellmate/repository/OfferRepository.java
+++ sellmate/src/main/java/com/example/sellmate/repository/OfferRepository.java
@@ -1,2 +1,11 @@
-package com.example.sellmate.repository;public interface OfferRepository {
+package com.example.sellmate.repository;
+
+import com.example.sellmate.entity.Offer;
+import com.example.sellmate.entity.enums.OfferStatus;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OfferRepository extends JpaRepository<Offer, Long> {
+    List<Offer> findByPostIdAndStatus(Long postId, OfferStatus status);
 }
