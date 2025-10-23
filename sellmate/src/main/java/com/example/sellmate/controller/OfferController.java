f3be0465d5610846eb24e92572f866116d0dbd17
berk cirak <atalayberkcrak@gmail.com>
2025-10-23 23:01:45 +0300

    feat: added offercontroller

--- sellmate/src/main/java/com/example/sellmate/controller/OfferController.java
+++ sellmate/src/main/java/com/example/sellmate/controller/OfferController.java
@@ -1,2 +1,37 @@
-package com.example.sellmate.controller;public class OfferController {
+package com.example.sellmate.controller;
+
+import com.example.sellmate.common.ApiResponse;
+import com.example.sellmate.dto.request.CreateOfferRequest;
+import com.example.sellmate.dto.response.OfferResponse;
+import com.example.sellmate.dto.response.OrderResponse;
+import com.example.sellmate.service.OfferService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/offers")
+public class OfferController {
+
+    private final OfferService offerService;
+
+    public OfferController(OfferService offerService) {
+        this.offerService = offerService;
+    }
+    @PostMapping("/posts/{postId}")
+    public ApiResponse<OfferResponse> createOffer(@PathVariable Long postId, @RequestBody CreateOfferRequest request, HttpServletRequest httpServletRequest){
+        OfferResponse offerResponse = offerService.createOffer(postId,request);
+        return ApiResponse.success(offerResponse, "Offer created successfully", httpServletRequest.getRequestURI());
+    }
+    @PostMapping("/{offerId}/accept")
+    public ApiResponse<OrderResponse> acceptOffer(@PathVariable Long offerId, HttpServletRequest httpServletRequest){
+        OrderResponse orderResponse = offerService.acceptOffer(offerId);
+        return ApiResponse.success(orderResponse, "Offer accepted", httpServletRequest.getRequestURI());
+    }
+    @PostMapping("{offerId}/reject")
+    public ApiResponse<OfferResponse> rejectOffer(@PathVariable Long offerId, HttpServletRequest httpServletRequest){
+        OfferResponse offerResponse = offerService.rejectOffer(offerId);
+        return ApiResponse.success(offerResponse, "Offer rejected", httpServletRequest.getRequestURI());
+    }
+
+
 }
