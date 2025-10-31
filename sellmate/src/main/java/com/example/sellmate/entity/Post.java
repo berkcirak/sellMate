df5c718107febd6d5925d9aff971d7207650fcf1
berk cirak <atalayberkcrak@gmail.com>
2025-10-31 16:08:53 +0300

    fix: resolved cascadetype problem

--- sellmate/src/main/java/com/example/sellmate/entity/Post.java
+++ sellmate/src/main/java/com/example/sellmate/entity/Post.java
@@ -20,7 +20,7 @@ public class Post extends BaseEntity {
     private BigDecimal price;
     @Column(name = "is_available", nullable = false)
     private Boolean isAvailable = true;
-    @ManyToOne(cascade = CascadeType.ALL)
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
     @ElementCollection
