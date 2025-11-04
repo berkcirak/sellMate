faa421cdb93755bfebd926090ea793ebfa4639e8
berk cirak <atalayberkcrak@gmail.com>
2025-11-04 23:21:35 +0300

    feat: added realtimenotifier

--- sellmate/src/main/java/com/example/sellmate/service/RealTimeNotifier.java
+++ sellmate/src/main/java/com/example/sellmate/service/RealTimeNotifier.java
@@ -1,2 +1,18 @@
-package com.example.sellmate.service;public class RealTimeNotifier {
+package com.example.sellmate.service;
+
+import com.example.sellmate.dto.response.NotificationResponse;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RealTimeNotifier {
+
+    private final SimpMessagingTemplate messagingTemplate;
+
+    public RealTimeNotifier(SimpMessagingTemplate messagingTemplate) {
+        this.messagingTemplate = messagingTemplate;
+    }
+    public void pushToUser(Long userId, NotificationResponse payload){
+        messagingTemplate.convertAndSend("/topic/users." + userId +".notifications", payload);
+    }
 }
