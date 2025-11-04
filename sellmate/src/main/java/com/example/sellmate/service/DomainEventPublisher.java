bd0aa564c0dfbdb9bdf8c78a459193f13649d24d
berk cirak <atalayberkcrak@gmail.com>
2025-11-04 23:22:11 +0300

    feat: added event publisher

--- sellmate/src/main/java/com/example/sellmate/service/DomainEventPublisher.java
+++ sellmate/src/main/java/com/example/sellmate/service/DomainEventPublisher.java
@@ -1,2 +1,32 @@
-package com.example.sellmate.service;public class DomainEventPublisher {
+package com.example.sellmate.service;
+
+import com.example.sellmate.event.*;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DomainEventPublisher {
+
+    private final RabbitTemplate rabbitTemplate;
+
+
+    public DomainEventPublisher(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+    public void publishLike(LikeCreatedEvent event){
+        rabbitTemplate.convertAndSend("notification.exchange", "notification.like", event);
+    }
+    public void publishComment(CommentCreatedEvent event){
+        rabbitTemplate.convertAndSend("notification.exchange", "notification.comment", event);
+    }
+    public void publishOrder(OrderPlacedEvent event){
+        rabbitTemplate.convertAndSend("notification.exchange", "notification.order", event);
+    }
+    public void publishOfferCreated(OfferCreatedEvent event){
+        rabbitTemplate.convertAndSend("notification.exchange", "notification.offer.created", event);
+    }
+    public void publishOfferDecision(OfferDecisionEvent event){
+        rabbitTemplate.convertAndSend("notification.exchange", "notification.offer.decision", event);
+    }
+
 }
