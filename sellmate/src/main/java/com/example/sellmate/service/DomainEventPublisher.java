3bf678624181d31ccb7f996f4cab42a6e8e280f1
berk cirak <atalayberkcrak@gmail.com>
2025-11-05 21:46:43 +0300

    feat: added log infos

--- sellmate/src/main/java/com/example/sellmate/service/DomainEventPublisher.java
+++ sellmate/src/main/java/com/example/sellmate/service/DomainEventPublisher.java
@@ -8,24 +8,30 @@ import org.springframework.stereotype.Service;
 public class DomainEventPublisher {
 
     private final RabbitTemplate rabbitTemplate;
+    private static final org.slf4j.Logger log = org.slf4j.LoggerFactory.getLogger(DomainEventPublisher.class);
 
 
     public DomainEventPublisher(RabbitTemplate rabbitTemplate) {
         this.rabbitTemplate = rabbitTemplate;
     }
     public void publishLike(LikeCreatedEvent event){
+        log.info("Publishing LIKE event: {}", event);
         rabbitTemplate.convertAndSend("notification.exchange", "notification.like", event);
     }
     public void publishComment(CommentCreatedEvent event){
+        log.info("Publishing COMMENT event: {}", event);
         rabbitTemplate.convertAndSend("notification.exchange", "notification.comment", event);
     }
     public void publishOrder(OrderPlacedEvent event){
+        log.info("Publishing ORDER event: {}", event);
         rabbitTemplate.convertAndSend("notification.exchange", "notification.order", event);
     }
     public void publishOfferCreated(OfferCreatedEvent event){
+        log.info("Publishing OFFER event: {}", event);
         rabbitTemplate.convertAndSend("notification.exchange", "notification.offer.created", event);
     }
     public void publishOfferDecision(OfferDecisionEvent event){
+        log.info("Publishing DECISION event: {}", event);
         rabbitTemplate.convertAndSend("notification.exchange", "notification.offer.decision", event);
     }
 
